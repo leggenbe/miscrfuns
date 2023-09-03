533d6328986b0a08cd35eda859f7fdf4bec1656f
leggenbe <lukas.eggenberger@uzh.ch>
2023-09-03 11:43:35 +0200

    change name from p_fun to p_form

--- R/p_form.R
+++ R/p_form.R
@@ -1,4 +1,14 @@
-p_fun <- function(x, lead = F, stars = T) {
+#' Format p-values Function
+#'
+#' This function allows you to format p-values w/o stars and leading/trailing zeroes.
+#' @param lead Should leading zeroes be shown? FALSE
+#' @param stars Should stars/asterisks be added? TRUE
+#' @keywords pvalues
+#' @export
+#' @examples
+#' p_form()
+
+p_form <- function(x, lead = F, stars = T) {
   ifelse(stars,
          case_when(x < 0.001 ~ paste0("<", ifelse(lead,"0.001***", ".001***")),
                    x < 0.01 ~ paste0(roundy(x, 3, lead), "**"),
